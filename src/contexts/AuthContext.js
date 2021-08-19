302718b00a0d8996e354af478eb1dd452b70ebdd
danadrol96 <danpark105@gmail.com>
2021-08-19 16:11:29 -0400

    adding firebase auth setup

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,68 @@
+import React, { useState, useContext, useEffect } from "react";
+import { auth } from "../firebase"; // comes from firebase.js
+
+const AuthContext = React.createContext();
+
+// Custom hook that allows us to use the AuthContext above
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  // These can be replaced with custom functions for a server if I decide not to go with firebase
+  const signup = (email, password) => {
+    return auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  const login = (email, password) => {
+    return auth.signInWithEmailAndPassword(email, password);
+  };
+
+  const logout = () => {
+    return auth.signOut();
+  };
+
+  const resetPassword = (email) => {
+    return auth.sendPasswordResetEmail(email);
+  };
+
+  const updateEmail = (email) => {
+    return currentUser.updateEmail(email);
+  };
+
+  const updatePassword = (password) => {
+    return currentUser.updatePassword(password);
+  };
+
+  useEffect(() => {
+    // Unsubscribe from the onAuthStateChanged function whenever component is unmounted
+    // Firebase sets localstorage and tokens to verify if a user is signed in
+    // That means there's an initial loading state
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
